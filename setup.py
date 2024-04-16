759c85272a0f2963dc2021354e71c39fb2d8edc3
Florian Deconinck <deconinck.florian@gmail.com>
2024-04-16 10:48:12 -0400

    Freezing mpi4py to 3.1.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_requires = {"test": test_requirements, "develop": develop_requirements}
 requirements: List[str] = [
     local_pkg("gt4py", "external/gt4py"),
     local_pkg("dace", "external/dace"),
-    "mpi4py",
+    "mpi4py==3.1.5",
     "cftime",
     "xarray",
     "f90nml>=1.1.0",
