aee7ade6ec04a60a429393671217108700e67c06
Florian Deconinck <deconinck.florian@gmail.com>
2024-12-24 11:56:23 -0500

    Move precision definition to `ndsl.dsl.__init__` Detect bad order of imports, e.g., gt4py import before NDSL Adapt code to use the newly defined NDSL_GLOBAL_PRECISION Set internal GT4Py literal precision definition

--- ndsl/dsl/__init__.py
+++ ndsl/dsl/__init__.py
@@ -1,6 +1,23 @@
-import gt4py.cartesian.config
+# Literal precision for both GT4Py & NDSL
+import os
+import sys
 
-from ndsl.comm.mpi import MPI
+
+gt4py_config_module = "gt4py.cartesian.config"
+if gt4py_config_module in sys.modules:
+    raise RuntimeError(
+        "`GT4Py` config imported before `ndsl` imported."
+        " Please import `ndsl.dsl` or any `ndsl` module "
+        " before any `gt4py` imports."
+    )
+NDSL_GLOBAL_PRECISION = int(os.getenv("PACE_FLOAT_PRECISION", "64"))
+os.environ["GT4PY_LITERAL_PRECISION"] = str(NDSL_GLOBAL_PRECISION)
+
+
+# Set cache names for default gt backends workflow
+import gt4py.cartesian.config  # noqa: E402
+
+from ndsl.comm.mpi import MPI  # noqa: E402
 
 
 if MPI is not None:
@@ -9,5 +26,3 @@ if MPI is not None:
     gt4py.cartesian.config.cache_settings["dir_name"] = os.environ.get(
         "GT_CACHE_DIR_NAME", f".gt_cache_{MPI.COMM_WORLD.Get_rank():06}"
     )
-
-__version__ = "0.2.0"
