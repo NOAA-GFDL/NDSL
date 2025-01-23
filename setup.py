3f77863467cc84b385ed6bc99c58da69ecbc00e1
Frank Malatino <frank.malatino@noaa.gov>
2025-01-23 11:42:54 -0500

    Updated version number in setup.py to reflect new release, 2025.01.00

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     packages=find_namespace_packages(include=["ndsl", "ndsl.*"]),
     include_package_data=True,
     url="https://github.com/NOAA-GFDL/NDSL",
-    version="2024.09.00",
+    version="2025.01.00",
     zip_safe=False,
     entry_points={
         "console_scripts": [
