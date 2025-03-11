Translate: Fix reading parameters with the right precision

ndsl/stencils/testing/translate.py
@@ -211,12 +211,7 @@ class TranslateFortranData2Py:
         if storage_vars is None:
             storage_vars = self.storage_vars()
         for p in self.in_vars["parameters"]:
-            if type(inputs_in[p]) in [int, np.int64, np.int32]:
-                inputs_out[p] = int(inputs_in[p])
-            elif type(inputs_in[p]) is bool:
-                inputs_out[p] = inputs_in[p]
-            else:
-                inputs_out[p] = Float(inputs_in[p])
+            inputs_out[p] = int(inputs_in[p])
         for d, info in storage_vars.items():
             serialname = info["serialname"] if "serialname" in info else d
             self.update_info(info, inputs_in)