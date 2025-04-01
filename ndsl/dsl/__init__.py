cedcb3e107a02d1a7db9183f91e5b0a132bc35e8
Charles Kropiewnicki <charles.j.krop@gmail.com>
2025-04-01 15:08:50 -0400

    Ability to import gt4py tools from within NDSL.
    gt4py can no longer be imported independently when using NDSL, it must be run through NDSL to ensure proper precision

--- ndsl/dsl/__init__.py
+++ ndsl/dsl/__init__.py
@@ -1,6 +1,24 @@
-import gt4py.cartesian.config
-
 from ndsl.comm.mpi import MPI
+
+# Literal precision for both GT4Py & NDSL
+import os
+import sys
+
+gt4py_config_module = "gt4py.cartesian.config"
+if gt4py_config_module in sys.modules:
+    raise RuntimeError(
+        "`GT4Py` config imported before `ndsl` imported."
+        " Please import `ndsl.dsl` or any `ndsl` module "
+        " before any `gt4py` imports."
+    )
+NDSL_GLOBAL_PRECISION = int(os.getenv("PACE_FLOAT_PRECISION", "64"))
+os.environ["GT4PY_LITERAL_PRECISION"] = str(NDSL_GLOBAL_PRECISION)
+
+
+# Set cache names for default gt backends workflow
+import gt4py.cartesian.config  # noqa: E402
+
+from ndsl.comm.mpi import MPI  # noqa: E402
 
 
 if MPI is not None:
