95ab6aa9fb6160870d74c6a6a3c32166a0de1afc
Florian Deconinck <deconinck.florian@gmail.com>
2025-04-17 09:45:03 -0400

    Log debugger on & config in debug

--- ndsl/debug/config.py
+++ ndsl/debug/config.py
@@ -1,6 +1,7 @@
 import os
 from ndsl.debug.debugger import Debugger
 from ndsl.comm.mpi import MPIComm
+from ndsl.logging import ndsl_log
 import yaml
 
 
@@ -15,6 +16,8 @@ def _set_debugger():
         config_dict = yaml.load(file.read(), Loader=yaml.SafeLoader)
     global ndsl_debugger
     ndsl_debugger = Debugger(rank=MPIComm().Get_rank(), **config_dict)
+    ndsl_log.info("[NDSL Debugger] On")
+    ndsl_log.debug(f"[NDSL Debugger] Config:\n{config_dict}")
 
 
 _set_debugger()
