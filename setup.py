23147a1ee4ccc3f61f28d9b7df9e3e701b0297b0
Frank Malatino <142349306+fmalatino@users.noreply.github.com>
2025-07-17 11:27:34 -0400

    Packaging: Use of pyproject.toml and move to minimal setup.py for modernization (#159)
    * Moving to pyproject.toml and minimal setup.py
    
    * Linting
    
    * Fixing setup.py syntax
    
    * Moving setup.cfg contents to pyproject.toml
    
    * Adding license information for ndsl/viz/fv3/README.md
    
    * Adding private classifier to pyproject.toml
    
    * Removing setup.cfg
    
    * Fixing unit test workflow
    
    * Changing install option from 'develop' to 'dev'
    
    * Adding linting for yamls
    
    * Adding Flake8-pyproject as additional dependency in pre-commit-config.yaml

--- setup.py
+++ setup.py
@@ -1,30 +1,14 @@
-import os
 from pathlib import Path
-from typing import List
 
-from setuptools import find_namespace_packages, setup
+from setuptools import setup
 
 
 def local_pkg(name: str, relative_path: str) -> str:
     """Returns an absolute path to a local package."""
-    path = f"{name} @ file://{Path(os.path.abspath(__file__)).parent / relative_path}"
-    return path
+    return f"{name} @ file://{Path(__file__).absolute().parent / relative_path}"
 
 
-docs_requirements = ["mkdocs-material"]
-demos_requirements = ["ipython", "ipykernel"]
-test_requirements = ["pytest", "pytest-subtests", "coverage"]
-
-develop_requirements = test_requirements + docs_requirements + ["pre-commit"]
-
-extras_requires = {
-    "demos": demos_requirements,
-    "develop": develop_requirements,
-    "docs": docs_requirements,
-    "test": test_requirements,
-}
-
-requirements: List[str] = [
+requirements: list[str] = [
     local_pkg("gt4py", "external/gt4py"),
     local_pkg("dace", "external/dace"),
     "mpi4py==3.1.5",
@@ -42,29 +26,4 @@ requirements: List[str] = [
 ]
 
 
-setup(
-    author="NOAA/NASA",
-    python_requires=">=3.11",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.11",
-    ],
-    install_requires=requirements,
-    extras_require=extras_requires,
-    name="ndsl",
-    license="Apache 2.0 license",
-    packages=find_namespace_packages(include=["ndsl", "ndsl.*"]),
-    include_package_data=True,
-    url="https://github.com/NOAA-GFDL/NDSL",
-    version="2025.05.00",
-    zip_safe=False,
-    entry_points={
-        "console_scripts": [
-            "ndsl-serialbox_to_netcdf = ndsl.stencils.testing.serialbox_to_netcdf:entry_point",
-        ]
-    },
-)
+setup(install_requires=requirements)
