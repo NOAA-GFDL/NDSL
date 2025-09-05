c6788d99dca304990bd0f4da053192044b17b705
Florian Deconinck <deconinck.florian@gmail.com>
2025-09-05 15:52:32 -0400

    [Orchestration] Common cast operation replacements (#211)
    * Replace `Float` by typecast during orch
    
    * Add `Int` to typecast + move import to a more respectable spot
    
    * Lint
    
    * Typos

---
+++ ndsl/dsl/dace/replacements.py
@@ -0,0 +1,32 @@
+"""This module uses DaCe's op_repository feature to override symbols/AST objects
+during parsing and replace them with an SDFG compatible representation. This
+allows custom NDSL syntax, objects and symbols to be natively orchestratable."""
+
+from dace import SDFG, SDFGState, dtypes
+from dace.frontend.common import op_repository as oprepo
+from dace.frontend.python.newast import ProgramVisitor
+from dace.frontend.python.replacements import UfuncInput, _datatype_converter
+
+from ndsl.dsl.typing import Float, Int
+
+
+@oprepo.replaces("Float")
+def _convert_Float(_pv: ProgramVisitor, sdfg: SDFG, state: SDFGState, arg: UfuncInput):
+    """Replace `Float(x)` with a typecast of `x` to the proper floating precision type"""
+    return _datatype_converter(
+        sdfg,
+        state,
+        arg,
+        dtype=dtypes.dtype_to_typeclass(Float),
+    )
+
+
+@oprepo.replaces("Int")
+def _convert_Int(_pv: ProgramVisitor, sdfg: SDFG, state: SDFGState, arg: UfuncInput):
+    """Replace `Int(x)` with a typecast of `x` to the proper integer precision type"""
+    return _datatype_converter(
+        sdfg,
+        state,
+        arg,
+        dtype=dtypes.dtype_to_typeclass(Int),
+    )
