64c4694112d13f01b39b8ad8a7c63dc81c22047e
Florian Deconinck <deconinck.florian@gmail.com>
2025-10-02 10:26:14 -0400

    State V0 (#242)
    * State v0 + utest
    
    * `dacite` as a new dependancy
    
    * Fix for `data` setter in quantity reseting the `compute_view` for `field` accessor
    
    * Lint
    
    * Fix circular import
    
    * Add update/init distinction
    Better docstings
    
    * Harden `quantity.data = ...` & add unit test
    
    * Use extent
    
    * Proper test fix
    
    * Lint
    
    * Better raise programming for `Quantity.data`
    
    * Better docs, naming for states. Improve quantity unit test

--- ndsl/__init__.py
+++ ndsl/__init__.py
@@ -28,7 +28,7 @@ from .namelist import Namelist
 from .performance.collector import NullPerformanceCollector, PerformanceCollector
 from .performance.profiler import NullProfiler, Profiler
 from .performance.report import Experiment, Report, TimeReport
-from .quantity import Quantity
+from .quantity import Quantity, State
 from .quantity.field_bundle import FieldBundle, FieldBundleType  # Break circular import
 from .testing.dummy_comm import DummyComm
 from .types import Allocator
@@ -48,7 +48,6 @@ __all__ = [
     "FV3CodePath",
     "DaceConfig",
     "DaCeOrchestration",
-    "FrozenCompiledSDFG",
     "orchestrate",
     "orchestrate_function",
     "ArrayReport",
@@ -87,4 +86,5 @@ __all__ = [
     "DummyComm",
     "Allocator",
     "MetaEnumStr",
+    "State",
 ]
