85aeaefa2f7f4dd8fad8674c4d50a71d090a3455
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2025-10-06 15:04:27 +0200

    Fix test and more cleanup

--- ndsl/monitor/netcdf_monitor.py
+++ ndsl/monitor/netcdf_monitor.py
@@ -1,9 +1,7 @@
 import os
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Set
 from warnings import warn
 
-import fsspec
 import numpy as np
 import xarray as xr
 
@@ -45,16 +43,14 @@ class _TimeChunkedVariable:
 class _ChunkedNetCDFWriter:
     FILENAME_FORMAT = "state_{chunk:04d}_tile{tile}.nc"
 
-    def __init__(
-        self, path: str, tile: int, fs: fsspec.AbstractFileSystem, time_chunk_size: int
-    ):
+    def __init__(self, path: str, tile: int, time_chunk_size: int) -> None:
         self._path = path
         self._tile = tile
         self._time_chunk_size = time_chunk_size
         self._i_time = 0
-        self._chunked: Optional[Dict[str, _TimeChunkedVariable]] = None
-        self._times: List[Any] = []
-        self._time_units: Optional[str] = None
+        self._chunked: dict[str, _TimeChunkedVariable] | None = None
+        self._times: list = []
+        self._time_units: str | None = None
 
     def append(self, state):
         ndsl_log.debug("appending at time %d", self._i_time)
@@ -128,8 +124,8 @@ class NetCDFMonitor:
         self._path = path
         self._communicator = communicator
         self._time_chunk_size = time_chunk_size
-        self.__writer: Optional[_ChunkedNetCDFWriter] = None
-        self._expected_vars: Optional[Set[str]] = None
+        self.__writer: _ChunkedNetCDFWriter | None = None
+        self._expected_vars: set[str] | None = None
         self._transfer_type = precision
         if self._transfer_type == np.float32 and get_precision() > 32:
             warn(
@@ -174,7 +170,7 @@ class NetCDFMonitor:
         if state is not None:  # we are on root rank
             self._writer.append(state)
 
-    def store_constant(self, state: Dict[str, Quantity]) -> None:
+    def store_constant(self, state: dict[str, Quantity]) -> None:
         state = self._communicator.gather_state(
             state, transfer_type=self._transfer_type
         )
