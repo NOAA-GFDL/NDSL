cc511fe1f288329de23a3c1fc4b06e2aaba2f03f
Florian Deconinck <deconinck.florian@gmail.com>
2025-10-07 11:50:26 -0400

    Move helper function for SDFG, delete unused code

--- ndsl/dsl/dace/sdfg/loop_transform.py
+++ ndsl/dsl/dace/sdfg/loop_transform.py
@@ -1,19 +1,19 @@
-from dace import SDFG, ScheduleType
+from dace import SDFG, ScheduleType, nodes
 
 
-def _make_sequential(sdfg: SDFG):
-    import dace
-
+def make_SDFG_CPU_sequential(sdfg: SDFG) -> None:
+    """Utility to turn a CPU-based SDFG to pure serial by removing OpenMP"""
     # Disable OpenMP sections
     for sd in sdfg.all_sdfgs_recursive():
         sd.openmp_sections = False
+
     # Disable OpenMP maps
-    for n, _ in sdfg.all_nodes_recursive():
-        if isinstance(n, dace.nodes.EntryNode):
-            sched = getattr(n, "schedule", False)
-            if sched in (
+    for node, _ in sdfg.all_nodes_recursive():
+        if isinstance(node, nodes.EntryNode):
+            schedule = getattr(node, "schedule", False)
+            if schedule in (
                 ScheduleType.CPU_Multicore,
                 ScheduleType.CPU_Persistent,
                 ScheduleType.Default,
             ):
-                n.schedule = dace.ScheduleType.Sequential
+                node.schedule = ScheduleType.Sequential
