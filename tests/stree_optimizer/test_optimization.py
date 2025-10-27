1c6edbb4f15a3ab6fc91835362663726151fdc66
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2025-10-27 10:30:57 +0100

    Unrelated: tests are supposed to return `None`

--- tests/stree_optimizer/test_optimization.py
+++ tests/stree_optimizer/test_optimization.py
@@ -6,18 +6,18 @@ from ndsl.dsl.gt4py import PARALLEL, computation, interval
 from ndsl.dsl.typing import FloatField
 
 
-def stencil_A(in_field: FloatField, out_field: FloatField):
+def stencil_A(in_field: FloatField, out_field: FloatField) -> None:
     with computation(PARALLEL), interval(...):
         out_field = in_field
 
 
-def stencil_B(in_field: FloatField, out_field: FloatField):
+def stencil_B(in_field: FloatField, out_field: FloatField) -> None:
     with computation(PARALLEL), interval(...):
         out_field = out_field + in_field * 3
 
 
 class TriviallyMergeableCode:
-    def __init__(self, stencil_factory: StencilFactory):
+    def __init__(self, stencil_factory: StencilFactory) -> None:
         orchestrate(obj=self, config=stencil_factory.config.dace_config)
         self.stencil_A = stencil_factory.from_dims_halo(
             func=stencil_A,
@@ -28,15 +28,15 @@ class TriviallyMergeableCode:
             compute_dims=[X_DIM, Y_DIM, Z_DIM],
         )
 
-    def __call__(self, in_field: FloatField, out_field: FloatField):
+    def __call__(self, in_field: FloatField, out_field: FloatField) -> None:
         self.stencil_A(in_field, out_field)
         self.stencil_B(in_field, out_field)
 
 
-def test_stree_roundtrip_no_opt():
+def test_stree_roundtrip_no_opt() -> None:
     """Dev Note:
 
-    The below code sucessfully merges top level K loop (2 loops)
+    The below code successfully merges top level K loop (2 loops)
     How do we test it?! Running doesn't test merging and the compilation
     is a near-black box. We could reach in the `dace_config.compiled_sdfg`
     cache but it's keyed on the dace.program and if we can reach the program
@@ -46,7 +46,7 @@ def test_stree_roundtrip_no_opt():
 
     Test is deactivated for now"""
 
-    return True
+    return
     domain = (3, 3, 4)
     stencil_factory, quantity_factory = get_factories_single_tile_orchestrated(
         domain[0], domain[1], domain[2], 0, backend="dace:cpu"
