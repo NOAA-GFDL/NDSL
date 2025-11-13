a6797955722d575ca269647532b663d94cbc0c91
Florian Deconinck <deconinck.florian@gmail.com>
2025-11-13 16:51:48 -0500

    Draft of more extended testing

--- tests/stree_optimizer/test_optimization.py
+++ tests/stree_optimizer/test_optimization.py
@@ -6,8 +6,22 @@ import ndsl.dsl.dace.orchestration as orch
 from ndsl import NDSLRuntime, Quantity, QuantityFactory, StencilFactory, orchestrate
 from ndsl.boilerplate import get_factories_single_tile_orchestrated
 from ndsl.constants import X_DIM, Y_DIM, Z_DIM
-from ndsl.dsl.gt4py import PARALLEL, computation, interval
+from ndsl.dsl.gt4py import FORWARD, PARALLEL, K, computation, interval
 from ndsl.dsl.typing import FloatField
+
+
+def _get_SDFG_and_purge(stencil_factory: StencilFactory) -> dace.CompiledSDFG:
+    """Get the Precompiled SDFG from and flush it out from the cache in order
+    for the function to be reused."""
+    sdfg_repo = stencil_factory.config.dace_config.loaded_precompiled_SDFG
+
+    if len(sdfg_repo.values()) != 1:
+        raise RuntimeError("Failure to compile SDFG")
+    sdfg = list(sdfg_repo.values())[0]
+
+    sdfg_repo.clear()
+
+    return sdfg
 
 
 class StreeOptimization:
@@ -26,61 +40,117 @@ class StreeOptimization:
         orch._INTERNAL__SCHEDULE_TREE_OPTIMIZATION = False
 
 
-def stencil_A(in_field: FloatField, out_field: FloatField) -> None:
+def copy_stencil(in_field: FloatField, out_field: FloatField) -> None:
     with computation(PARALLEL), interval(...):
-        out_field = in_field
+        out_field = in_field + 1
 
 
-def stencil_B(in_field: FloatField, out_field: FloatField) -> None:
+def copy_stencil_with_self_assign(in_field: FloatField, out_field: FloatField) -> None:
     with computation(PARALLEL), interval(...):
-        out_field = out_field + in_field * 3
+        out_field = out_field + in_field + 2
+
+
+def copy_stencil_with_forward_K(in_field: FloatField, out_field: FloatField) -> None:
+    with computation(FORWARD), interval(...):
+        out_field = in_field + 3
+
+
+def copy_stencil_with_if(in_field: FloatField, out_field: FloatField) -> None:
+    with computation(PARALLEL), interval(...):
+        if K > 3:
+            out_field = in_field + 4
+
+
+def copy_stencil_with_temp_and_offset_in_K(
+    in_field: FloatField,
+    out_field: FloatField,
+) -> None:
+    with computation(PARALLEL), interval(...):
+        temporary_field = in_field + 5
+
+    with computation(PARALLEL), interval(0, -1):
+        out_field = temporary_field[K - 1] + 6
 
 
 class TriviallyMergeableCode:
     def __init__(self, stencil_factory: StencilFactory) -> None:
         orchestrate(obj=self, config=stencil_factory.config.dace_config)
-        self.stencil_A = stencil_factory.from_dims_halo(
-            func=stencil_A,
-            compute_dims=[X_DIM, Y_DIM, Z_DIM],
-        )
-        self.stencil_B = stencil_factory.from_dims_halo(
-            func=stencil_B,
+        self.stencil = stencil_factory.from_dims_halo(
+            func=copy_stencil,
             compute_dims=[X_DIM, Y_DIM, Z_DIM],
         )
 
     def __call__(self, in_field: FloatField, out_field: FloatField) -> None:
-        self.stencil_A(in_field, out_field)
-        self.stencil_B(in_field, out_field)
+        self.stencil(in_field, out_field)
+        self.stencil(in_field, out_field)
+
+
+class NonTrivialMergingCode:
+    def __init__(self, stencil_factory: StencilFactory) -> None:
+        orchestrate(obj=self, config=stencil_factory.config.dace_config)
+        self.copy_stencil = stencil_factory.from_dims_halo(
+            func=copy_stencil,
+            compute_dims=[X_DIM, Y_DIM, Z_DIM],
+        )
+        self.copy_stencil_with_forward_K = stencil_factory.from_dims_halo(
+            func=copy_stencil_with_forward_K,
+            compute_dims=[X_DIM, Y_DIM, Z_DIM],
+        )
+        self.copy_stencil_with_if = stencil_factory.from_dims_halo(
+            func=copy_stencil_with_if,
+            compute_dims=[X_DIM, Y_DIM, Z_DIM],
+        )
+        self.copy_stencil_with_temp_and_offset_in_K = stencil_factory.from_dims_halo(
+            func=copy_stencil_with_temp_and_offset_in_K,
+            compute_dims=[X_DIM, Y_DIM, Z_DIM],
+        )
+
+    def __call__(self, in_field: FloatField, out_field: FloatField) -> None:
+        self.copy_stencil(in_field, out_field)
+        self.copy_stencil_with_if(in_field, out_field)
+        self.copy_stencil_with_forward_K(in_field, out_field)
+        # self.copy_stencil_with_temp_and_offset_in_K(in_field, out_field)
 
 
 def test_stree_merge_maps() -> None:
     domain = (3, 3, 4)
     stencil_factory, quantity_factory = get_factories_single_tile_orchestrated(
-        domain[0], domain[1], domain[2], 0, backend="dace:cpu"
+        domain[0], domain[1], domain[2], 0, backend="dace:cpu_kfirst"
     )
 
-    code = TriviallyMergeableCode(stencil_factory)
+    trivial_code = TriviallyMergeableCode(stencil_factory)
     in_qty = quantity_factory.ones([X_DIM, Y_DIM, Z_DIM], "")
     out_qty = quantity_factory.zeros([X_DIM, Y_DIM, Z_DIM], "")
 
-    with StreeOptimization():
-        code(in_qty, out_qty)
+    # with StreeOptimization():
+    #     trivial_code(in_qty, out_qty)
+    #     precompiled_sdfg = _get_SDFG_and_purge(stencil_factory)
+    #     all_maps = [
+    #         (me, state)
+    #         for me, state in precompiled_sdfg.sdfg.all_nodes_recursive()
+    #         if isinstance(me, dace.nodes.MapEntry)
+    #     ]
 
-        assert (
-            len(stencil_factory.config.dace_config.loaded_precompiled_SDFG.values())
-            == 1
-        )
-        sdfg = list(
-            stencil_factory.config.dace_config.loaded_precompiled_SDFG.values()
-        )[0]
+    #     assert len(all_maps) == 3
+    #     assert (out_qty.field[:] == 1).all()
+
+    complex_code = NonTrivialMergingCode(stencil_factory)
+    with StreeOptimization():
+        complex_code(in_qty, out_qty)
+        sdfg = _get_SDFG_and_purge(stencil_factory).sdfg
         all_maps = [
             (me, state)
-            for me, state in sdfg.sdfg.all_nodes_recursive()
+            for me, state in sdfg.all_nodes_recursive()
             if isinstance(me, dace.nodes.MapEntry)
         ]
-
         assert len(all_maps) == 3
-        assert (out_qty.field[:] == 4).all()
+        all_loop_guard_state = [
+            (me, state)
+            for me, state in sdfg.all_nodes_recursive()
+            if isinstance(me, dace.SDFGState) and me.name.startswith("loop_guard")
+        ]
+        assert len(all_loop_guard_state) == 1
+        print(sdfg)
 
 
 class LocalRefineableCode(NDSLRuntime):
@@ -89,7 +159,7 @@ class LocalRefineableCode(NDSLRuntime):
     ) -> None:
         super().__init__(stencil_factory.config.dace_config)
         self.stencil_A = stencil_factory.from_dims_halo(
-            func=stencil_A,
+            func=copy_stencil,
             compute_dims=[X_DIM, Y_DIM, Z_DIM],
         )
         self.tmp = self.make_local(quantity_factory, [X_DIM, Y_DIM, Z_DIM])
@@ -112,14 +182,8 @@ def test_stree_roundtrip_transient_is_refined() -> None:
     with StreeOptimization():
         code(in_qty, out_qty)
 
-        assert (
-            len(stencil_factory.config.dace_config.loaded_precompiled_SDFG.values())
-            == 1
-        )
-        sdfg = list(
-            stencil_factory.config.dace_config.loaded_precompiled_SDFG.values()
-        )[0]
+        precompiled_sdfg = _get_SDFG_and_purge(stencil_factory)
 
-        for array in sdfg.sdfg.arrays.values():
+        for array in precompiled_sdfg.sdfg.arrays.values():
             if array.transient:
                 assert array.shape == (1, 1, 1)
