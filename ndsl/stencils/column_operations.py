b4d109e2c07311bb207996489849ca073e6a1766
Charles Kropiewnicki <79879064+CharlesKrop@users.noreply.github.com>
2025-11-14 13:51:51 -0500

    Added new functions: column_min_ddim & column_max_ddim and cooresponding test (#324)
    Functionality is the same as column_min/max, but separate functions are needed to handle cases with off grid data dimensions

--- ndsl/stencils/column_operations.py
+++ ndsl/stencils/column_operations.py
@@ -30,6 +30,31 @@ def column_max(field, start_index, end_index):
 
 @typing.no_type_check
 @function
+def column_max_ddim(field, ddim, start_index, end_index):
+    """
+    Find the maximum value for a full or slice of a column.
+
+    Args:
+        field: data to be analyzed
+        start_index: "bottom" index of slice, must be less than end_index
+        end_index: "top" index of slice, must be greater than start_index
+
+        Returns: [max value, index of max value]
+    """
+    max_index = 0
+    level = start_index
+    while level <= end_index:
+        new = field.at(K=level, ddim=[ddim])
+        old = field.at(K=max_index, ddim=[ddim])
+        if new > old:
+            max_index = level
+        level += 1
+
+    return field.at(K=max_index, ddim=[ddim]), max_index
+
+
+@typing.no_type_check
+@function
 def column_min(field, start_index, end_index):
     """
     Find the minimum value for a full or slice of a column.
@@ -51,3 +76,28 @@ def column_min(field, start_index, end_index):
         level += 1
 
     return field.at(K=min_index), min_index
+
+
+@typing.no_type_check
+@function
+def column_min_ddim(field, ddim, start_index, end_index):
+    """
+    Find the minimum value for a full or slice of a column.
+
+    Args:
+        field: data to be analyzed
+        start_index: "bottom" index of slice, must be less than end_index
+        end_index: "top" index of slice, must be greater than start_index
+
+        Returns: [min value, index of min value]
+    """
+    min_index = 0
+    level = start_index
+    while level <= end_index:
+        new = field.at(K=level, ddim=[ddim])
+        old = field.at(K=min_index, ddim=[ddim])
+        if new < old:
+            min_index = level
+        level += 1
+
+    return field.at(K=min_index, ddim=[ddim]), min_index
