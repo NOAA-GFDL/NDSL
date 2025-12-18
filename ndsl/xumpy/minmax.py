0ae7a132afd8ca7b0a9980469c0eb3d18899a5ec
Florian Deconinck <deconinck.florian@gmail.com>
2025-12-18 12:12:38 -0500

    Add `min_on_horizontal_place` and fix docstring

--- ndsl/xumpy/minmax.py
+++ ndsl/xumpy/minmax.py
@@ -1,15 +1,11 @@
+from functools import singledispatch
+
 import numpy as np
 import numpy.typing as npt
 
+from ndsl import Quantity
 from ndsl.dsl.typing import Float
 from ndsl.optional_imports import cupy as cp
-
-
-if cp is None:
-    cp = np
-from functools import singledispatch
-
-from ndsl import Quantity
 
 
 @singledispatch
@@ -19,20 +15,41 @@ def max_on_horizontal_plane(
 ) -> None:
     """Find maximum value on each horizontal plane, e.g. for the
     first two dimensions of the array."""
-    raise NotImplementedError("")
+    raise NotImplementedError(
+        f"max_on_horizontal_plane called with unsupported type {type(in_buffer)}"
+    )
 
 
 @max_on_horizontal_plane.register(np.ndarray | cp.ndarray)
 def _(in_buffer: npt.NDArray, out_buffer: npt.NDArray) -> None:
-    print("numpy/cupy")
     out_buffer[:] = in_buffer.max(axis=(0, 1))
 
 
 @max_on_horizontal_plane.register(Quantity)
 def _(in_quantity: Quantity, out_quantity: Quantity) -> None:
-    print("qty")
-
     out_quantity.field[:] = in_quantity.field.max(axis=(0, 1))
+
+
+@singledispatch
+def min_on_horizontal_plane(
+    in_buffer: npt.NDArray | Quantity,
+    out_buffer: npt.NDArray | Quantity,
+) -> None:
+    """Find minimum value on each horizontal plane, e.g. for the
+    first two dimensions of the array."""
+    raise NotImplementedError(
+        f"min_on_horizontal_plane called with unsupported type {type(in_buffer)}"
+    )
+
+
+@min_on_horizontal_plane.register(np.ndarray | cp.ndarray)
+def _(in_buffer: npt.NDArray, out_buffer: npt.NDArray) -> None:
+    out_buffer[:] = in_buffer.min(axis=(0, 1))
+
+
+@min_on_horizontal_plane.register(Quantity)
+def _(in_quantity: Quantity, out_quantity: Quantity) -> None:
+    out_quantity.field[:] = in_quantity.field.min(axis=(0, 1))
 
 
 @singledispatch
@@ -40,7 +57,7 @@ def max(
     buffer: npt.NDArray | Quantity | int,
     axis: int | tuple[int, ...] | None = None,
 ) -> Float:
-    """Find maximum value on a 1D field."""
+    """Find maximum value."""
     raise NotImplementedError(f"Missing implementation for {type(buffer)}")
 
 
@@ -65,8 +82,8 @@ def min(
     buffer: npt.NDArray | Quantity | int,
     axis: int | tuple[int, ...] | None = None,
 ) -> Float:
-    """Find minimum value on a 1D field."""
-    raise NotImplementedError("")
+    """Find minimum value."""
+    raise NotImplementedError(f"Missing implementation for {type(buffer)}")
 
 
 @min.register(np.ndarray | cp.ndarray)
