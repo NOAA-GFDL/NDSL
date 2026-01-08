155c2d78eccb99adf1ffaf9230c7d8d8049a1057
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2026-01-08 17:59:11 +0100

    BREAKING CHANGE: remove deprecated QuanityFactory.from_backend()
    `QuanityFactory.from_backend(...)` was deprecated with NDSL release
    `2025.11.00`. The function is superseded by the new constructor. See
    https://github.com/NOAA-GFDL/NDSL/pull/228 for context.

--- tests/initialization/test_allocator.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from ndsl import QuantityFactory
-
-
-def test_QuantityFactory_from_backend_warns() -> None:
-    with pytest.deprecated_call():
-        QuantityFactory.from_backend(None, backend="numpy")
