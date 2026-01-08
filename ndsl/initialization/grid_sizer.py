faef100efe42626f35fed6eb0ea44c3d0e4e09b6
Florian Deconinck <deconinck.florian@gmail.com>
2026-01-08 14:10:28 -0500

    Remove dead code

--- ndsl/initialization/grid_sizer.py
+++ ndsl/initialization/grid_sizer.py
@@ -15,8 +15,6 @@ class GridSizer(ABC):
     """Number of horizontal halo points for produced arrays."""
     data_dimensions: dict[str, int]
     """Name/Lengths pair of any non-x/y/z dimensions, such as land or radiation dimensions."""
-    internal_pad_non_interface_dimensions: bool = False
-    """Enforce interface and non-interface dimensions true memory to be the same"""
 
     @abstractmethod
     def get_origin(self, dims: Sequence[str]) -> tuple[int, ...]: ...
