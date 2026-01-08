2bcd827910dbef69757888b22eb518fbb2b2b149
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2026-01-08 18:20:07 +0100

    BREAKING CHANGE: `Quantity` & `QuantityMetadata` changes
    Remove deprecation from `Quanity` and `QuantityMetadata`. This includes
    
    - `gt4py_backend` is superseded by just `backend`
    - the `backend` argument/member is required now
    - data must be allocated by either numpy or cupy

--- ndsl/quantity/metadata.py
+++ ndsl/quantity/metadata.py
@@ -16,40 +16,39 @@ if cupy is None:
 @dataclasses.dataclass
 class QuantityMetadata:
     origin: tuple[int, ...]
-    "the start of the computational domain"
+    "The start of the computational domain."
     extent: tuple[int, ...]
-    "the shape of the computational domain"
+    "The shape of the computational domain."
     n_halo: int
-    "Number of halo-points used in the horizontal"
+    "Number of halo-points used in the horizontal."
     dims: tuple[str, ...]
-    "names of each dimension"
+    "Names of each dimension."
     units: str
-    "units of the quantity"
+    "Units of the quantity."
     data_type: type
-    "ndarray-like type used to store the data"
+    "ndarray-like type used to store the data."
     dtype: type
-    "dtype of the data in the ndarray-like object"
-    gt4py_backend: str | None = None
-    "Deprecated. Use backend instead."
-    backend: str | None = None
+    "dtype of the data in the ndarray-like object."
+    backend: str
     "GT4Py backend name. Used for performance optimal data allocation."
 
     @property
     def dim_lengths(self) -> dict[str, int]:
-        """mapping of dimension names to their lengths"""
+        """Mapping of dimension names to their lengths."""
         return dict(zip(self.dims, self.extent))
 
     @property
     def np(self) -> NumpyModule:
-        """numpy-like module used to interact with the data"""
+        """numpy-like module used to interact with the data."""
         if issubclass(self.data_type, cupy.ndarray):
             return cupy
-        elif issubclass(self.data_type, np.ndarray):
+
+        if issubclass(self.data_type, np.ndarray):
             return np
-        else:
-            raise TypeError(
-                f"quantity underlying data is of unexpected type {self.data_type}"
-            )
+
+        raise TypeError(
+            f"Quantity underlying data is of unexpected type {self.data_type}"
+        )
 
     def duplicate_metadata(self, metadata_copy: QuantityMetadata) -> None:
         metadata_copy.origin = self.origin
@@ -58,7 +57,6 @@ class QuantityMetadata:
         metadata_copy.units = self.units
         metadata_copy.data_type = self.data_type
         metadata_copy.dtype = self.dtype
-        metadata_copy.gt4py_backend = self.gt4py_backend
         metadata_copy.backend = self.backend
 
 
--- tests/quantity/test_local.py
+++ tests/quantity/test_local.py
@@ -31,41 +31,6 @@ def test_dace_data_descriptor_is_transient() -> None:
     assert array.transient
 
 
-def test_gt4py_backend_is_deprecated() -> None:
-    nx = 5
-    shape = (nx,)
-    backend = "debug"
-    with pytest.deprecated_call(match="gt4py_backend is deprecated"):
-        local = Local(
-            data=np.empty(shape),
-            origin=(0,),
-            extent=(nx,),
-            dims=("dim_X",),
-            units="n/a",
-            gt4py_backend=backend,
-        )
-
-    # make sure we assign backend
-    assert local.backend == backend
-
-    # make sure we are backwards compatible (for now)
-    with pytest.deprecated_call(match="gt4py_backend is deprecated"):
-        assert local.gt4py_backend == backend
-
-
-def test_backend_will_be_required() -> None:
-    nx = 5
-    shape = (nx,)
-    with pytest.deprecated_call(match="`backend` will be a required argument"):
-        local = Local(
-            data=np.empty(shape),
-            origin=(0,),
-            extent=(nx,),
-            dims=("dim_X",),
-            units="n/a",
-        )
-
-
 @dataclasses.dataclass
 class GoodLocals(LocalState):
     my_local: Local = dataclasses.field(
