e83a3b5fa05b2443c71adcc3ea9b8c0db0cb710a
rem1776 <Ryan.Mulhall@noaa.gov>
2026-01-22 13:21:19 -0500

    add initial monitor obj and test for diag manager

---
+++ ndsl/monitor/diag_manager_monitor.py
@@ -0,0 +1,182 @@
+import os
+from pathlib import Path
+from warnings import warn
+
+import numpy as np
+import xarray as xr
+
+from ndsl.comm.communicator import Communicator
+from ndsl.dsl.typing import Float, get_precision
+from ndsl.logging import ndsl_log
+from ndsl.monitor.convert import to_numpy
+from ndsl.quantity import Quantity
+from ndsl.monitor.netcdf_monitor import NetCDFMonitor
+
+from ndsl.constants import N_HALO_DEFAULT
+from ndsl import GridSizer
+from ndsl.quantity.state import State
+    
+
+from pyfms import diag_manager, fms, mpp_domains
+
+from mpi4py import MPI
+
+
+class DiagManagerMonitor(NetCDFMonitor):
+    """
+    sympl.Monitor-style object for sending diagnostics to FMS's diag manager
+    """
+
+    _CONSTANT_FILENAME = "constants"
+
+    def __init__(  # type: ignore[no-untyped-def]
+        self,
+        path: str,
+        communicator: Communicator,
+        domain_id: int,
+        time_chunk_size: int = 1,
+        precision=Float,
+    ) -> None:
+        """Create a DiagManagerMonitor. Generates its own namelist that requires  
+
+        Args:
+            path: directory in which to store data
+            communicator: provides global communication to gather state
+            time_chunk_size: number of times per file
+        """
+        # creates a namelist for the diag manager to run, need to enable yaml support
+        text_content = "&diag_manager_nml\nuse_modern_diag=.true.\n/"
+        with open("input.nml", "w", encoding="utf-8") as f:
+            f.write(text_content)
+        # then initialize diag_manager and this object, mpp domain should be created prior to this
+        mpp_domains.set_current_domain(domain_id=domain_id)
+        diag_manager.init(diag_model_subset=diag_manager.DIAG_ALL)
+        self.domain_id = domain_id
+        self.fields = {}
+        self.axes = {}
+        super(DiagManagerMonitor, self).__init__(path, communicator, time_chunk_size, precision)
+
+
+    def store(self, state: dict) -> None:
+        """Append the model state dictionary to the netcdf files.
+
+        Will only write to disk when a full time chunk has been accumulated,
+        or when .cleanup() is called.
+
+        Requires the state contain the same quantities with the same metadata as the
+        first time this is called. Dimension order metadata may change between calls
+        so long as the set of dimensions is the same. Quantities are stored with
+        dimensions [time, tile] followed by the dimensions included in the first
+        state snapshot. The one exception is "time" which is stored with dimensions
+        [time].
+        """
+        if self._expected_vars is None:
+            self._expected_vars = set(state.keys())
+        elif self._expected_vars != set(state.keys()):
+            raise ValueError(
+                "state keys must be the same each time store is called, "
+                "got {} but previously got {}".format(
+                    set(state.keys()), self._expected_vars
+                )
+            )
+        # diag manager expects data from each PE, shouldn't need to gather state but we'll see
+        #state = self._communicator.tile.gather_state(
+        #    state, transfer_type=self._transfer_type
+        #)
+
+        # if no state then we have nothing to do
+        if state is None:
+            return
+
+        # get the associated quantities/axis for each field that has been registered
+        if state is not None:
+            for field_name, field_id in self.fields:
+               field_quantity = getattr(state, field_name) # this may not work
+               axis_ids = list(map(lambda dimname: axis_ids[dimname], field_quantity.dims))
+               # TODO data conversion may not be correct here
+               diag_manager.send_data(diag_field_id=field_id, field=np.ascontiguousarray(field_quantity.data), convert_cf_order=True)
+               diag_manager.send_complete(field_id)
+               diag_manager.advance_field_time(field_id)
+
+
+    def cleanup(self):
+        diag_manager.end()
+
+    # sets the end/initial times that the run is using in the diag manager 
+    def set_model_times(self, init_time, end_time):
+        diag_manager.set_field_init_time(
+            year=init_time.year,
+            month=init_time.month,
+            day=init_time.day,
+            hour=init_time.hour,
+            minute=init_time.minute,
+            second=init_time.second,
+        )
+        diag_manager.set_time_end(
+            year=end_time.year,
+            month=end_time.month,
+            day=end_time.day,
+            hour=end_time.hour,
+            minute=end_time.minute,
+            second=end_time.second,
+        )
+
+    # registers a diag_manager field/variable for a given Quantity, adds name:field_id to self.fields
+    def register_field(self, name: str, quantity: Quantity, domain_id: int, timestep):
+        self.fields[name] = diag_manager.field_init(
+            name=name,
+            long_name=name,
+            units=quantity.units,
+            domain_id=domain_id,
+            set_name="atm",
+            cart_name=name,
+            precision=get_precision(quantity.data),
+        )
+
+    # registers a axis in diag_manager, adds name:axis_id to self.axes 
+    def register_axis(self, name: str, axis_data: np.ndarray, cart_name: str = None,
+                      long_name: str = None, units: str = None):
+        self.axes[name] = diag_manager.axis_init(
+            name=name,
+            long_name=long_name,
+            axis_data=axis_data,
+            cart_name=cart_name,
+            domain_id=self.domain_id,
+            set_name="atm",
+            units="radians"
+        )
+
+        
+# this is a copy of the _TimeChunkedVariable from netcdf_monitor.py for now, intent is to modify for fms/fortran formatting later as needed 
+class _FortranFormattedVariable:
+    '''
+    Handles adjusting variable data from a Quantity to be passed into Fortran routines for pyFMS
+    '''
+    def __init__(self, initial: Quantity, time_chunk_size: int):
+        self._data = np.zeros(
+            (time_chunk_size, *initial.extent), dtype=initial.data.dtype
+        )
+        self._data[0, ...] = to_numpy(initial.view[:])
+        self._dims = initial.dims
+        self._units = initial.units
+        self._i_time = 1
+        self._backend = initial.backend
+
+    def append(self, quantity: Quantity) -> None:
+        # Allow mismatch precision here since this is I/O
+        self._data[self._i_time, ...] = to_numpy(
+            quantity.transpose(self._dims, allow_mismatch_float_precision=True).view[:]
+        )
+        self._i_time += 1
+
+    @property
+    def data(self) -> Quantity:
+        # Allow mismatch precision here since this is I/O
+        return Quantity(
+            data=self._data[: self._i_time, ...],
+            dims=("time",) + tuple(self._dims),
+            units=self._units,
+            allow_mismatch_float_precision=True,
+            backend=self._backend,
+        )
+
---
+++ tests/test_diag_manager_monitor.py
@@ -0,0 +1,192 @@
+import logging
+from datetime import timedelta
+from typing import List
+
+import cftime
+import numpy as np
+import pytest
+import xarray as xr
+import mpi4py.MPI as MPI
+
+from ndsl import (
+    CubedSphereCommunicator,
+    CubedSpherePartitioner,
+    LocalComm,
+    Quantity,
+    TilePartitioner,
+    DiagManagerMonitor,
+)
+
+from pyfms import mpp_domains, mpp, fms
+
+from pathlib import Path
+
+
+logger = logging.getLogger(__name__)
+
+
+# TODO fix the parametrizations once the test works 
+#@pytest.mark.parametrize("layout", [(1, 1), (1, 2), (4, 4)])
+#@pytest.mark.parametrize(
+#    "nt, time_chunk_size",
+#    [pytest.param(1, 1, id="single_time"), pytest.param(5, 2, id="chunked_time")],
+#)
+#@pytest.mark.parametrize(
+#    "shape, ny_rank_add, nx_rank_add, dims",
+#    [
+#        pytest.param((5, 4, 4), 0, 0, ("z", "y", "x"), id="cell_center"),
+#        pytest.param(
+#            (5, 4, 4), 1, 1, ("z", "y_interface", "x_interface"), id="cell_corner"
+#        ),
+#        pytest.param((5, 4, 4), 0, 1, ("z", "y", "x_interface"), id="cell_edge"),
+#    ],
+#)
+#def test_monitor_store_multi_rank_state(
+#    layout, nt, time_chunk_size, tmpdir, shape, ny_rank_add, nx_rank_add, dims, numpy
+#):
+
+def test_monitor_store_multi_rank_state():
+    units = "m"
+    backend = "debug"
+    nz, ny, nx = (16, 16, 1)
+    nt = 3
+    layout = (1, 1)
+    ny_rank = int(ny / layout[0]) # + ny_rank_add)
+    nx_rank = int(nx / layout[1])  # + nx_rank_add)
+    tile = TilePartitioner(layout)
+    time = cftime.DatetimeJulian(2010, 6, 20, 6, 0, 0)
+    timestep = timedelta(hours=1)
+    total_ranks = 6 * layout[0] * layout[1]
+    partitioner = CubedSpherePartitioner(tile)
+    shared_buffer = {}
+    monitor_list: List[DiagManagerMonitor] = []
+    tmpdir = "./diag_manager_monitor_test/"
+    dims = ("z", "y", "x")
+
+    domain_id = fms_mpp_init(cubed_sphere=False)
+
+    for rank in range(total_ranks):
+        communicator = CubedSphereCommunicator(
+            partitioner=partitioner,
+            comm=LocalComm(
+                rank=rank, total_ranks=total_ranks, buffer_dict=shared_buffer
+            ),
+        )
+        # must eagerly initialize the tile object so that their ranks are
+        # created in ascending order
+        communicator.tile
+        monitor_list.append(
+            DiagManagerMonitor(
+                path=tmpdir,
+                communicator=communicator,
+                time_chunk_size=1,
+                domain_id=domain_id,
+            )
+        )
+    # TODO constant storage
+    for rank in range(total_ranks - 1, -1, -1):
+        state = {
+            "var_const1": Quantity(
+                np.ones([nz, ny_rank, nx_rank]),
+                dims=dims,
+                units=units,
+                backend=backend,
+            ),
+        }
+    #    monitor_list[rank].store_constant(state)
+
+    tile_gathered = []
+    for i_t in range(nt):
+        for rank in range(total_ranks - 1, -1, -1):
+            state = {
+                "time": time + i_t * timestep,
+                "var1": Quantity(
+                    np.ones([nz, ny_rank, nx_rank]),
+                    dims=dims,
+                    units=units,
+                    backend=backend,
+                ),
+            }
+            monitor_list[rank].store(state)
+            tile_gathered.append(
+                monitor_list[rank]._communicator.tile.gather_state(state)
+            )
+
+    for rank in range(total_ranks - 1, -1, -1):
+        state = {
+            "var_const2": Quantity(
+                np.ones([nz, ny_rank, nx_rank]),
+                dims=dims,
+                units=units,
+                backend=backend,
+            ),
+        }
+    #    monitor_list[rank].store_constant(state)
+
+    for monitor in monitor_list:
+        monitor.cleanup()
+
+    # TODO output checking
+    #ds = xr.open_mfdataset(str(tmpdir / "state_*_tile*.nc"), decode_times=True)
+    #assert "var1" in ds
+    #np.testing.assert_array_equal(
+        #ds["var1"].shape, (nt, 6, nz, ny + ny_rank_add, nx + nx_rank_add)
+    #)
+    #assert ds["var1"].dims == ("time", "tile") + dims
+    #assert ds["var1"].attrs["units"] == units
+    #assert ds["time"].shape == (nt,)
+    #assert ds["time"].dims == ("time",)
+    #assert ds["time"].values[0] == time
+    #np.testing.assert_array_equal(ds["var1"].values, 1.0)
+
+    #ds_const = xr.open_dataset(str(tmpdir / "constants_var_const1.nc"))
+    #assert "var_const1" in ds_const
+    #np.testing.assert_array_equal(
+        #ds_const["var_const1"].shape, (6, nz, ny + ny_rank_add, nx + nx_rank_add)
+    #)
+    #assert ds_const["var_const1"].dims == ("tile",) + dims
+    #assert ds_const["var_const1"].attrs["units"] == units
+    #np.testing.assert_array_equal(ds_const["var_const1"].values, 1.0)
+    #ds_const2 = xr.open_dataset(str(tmpdir / "constants_var_const2.nc"))
+    #assert "var_const2" in ds_const2
+    #np.testing.assert_array_equal(
+        #ds_const2["var_const2"].shape, (6, nz, ny + ny_rank_add, nx + nx_rank_add)
+    #)
+    #assert ds_const2["var_const2"].dims == ("tile",) + dims
+    #assert ds_const2["var_const2"].attrs["units"] == units
+    #np.testing.assert_array_equal(ds_const2["var_const2"].values, 1.0)
+
+
+# init fms mpi and set up a simple domain
+def fms_mpp_init(cubed_sphere:bool=False):
+    nhalo = 1
+    x = 16
+    y = 16 
+
+    fms.init(localcomm=MPI.COMM_WORLD.py2f(), calendar_type=fms.NOLEAP)
+
+    if not cubed_sphere:
+        layout = [1, 1]
+        io_layout = [1, 1]
+        global_indices = [0, x-1, 0, y-1]
+        halo = 1 
+
+        domain = mpp_domains.define_domains(
+            global_indices=global_indices,
+            layout=layout,
+            whalo=halo,
+            ehalo=halo,
+            shalo=halo,
+            nhalo=halo,
+            xflags=mpp_domains.CYCLIC_GLOBAL_DOMAIN,
+            yflags=mpp_domains.CYCLIC_GLOBAL_DOMAIN,
+        )
+
+        id_num = domain.domain_id
+        mpp_domains.define_io_domain(
+            domain_id=id_num,
+            io_layout=io_layout,
+        )
+        return id_num
+    else:
+        raise NotImplementedError("Cubed sphere FMS MPP init not implemented yet")
