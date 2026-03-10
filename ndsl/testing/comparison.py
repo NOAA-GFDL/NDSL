fixed the changing points calculations, added changing column calculation, and updated the log within the comparision calculations of the translate test

ndsl/testing/comparison.py
@@ -261,8 +261,12 @@ class MultiModalFloatMetric(BaseMetric):
         # all the way down, we bail out if we can measure input vs reference
         if input_values is not None and input_values.shape == reference_values.shape:
             self.number_changing_values = (
-                (input_values != reference_values).flatten().shape[0]
+                (input_values != reference_values).sum()
             )
+            if len(input_values.shape) == 3:
+                self.changing_column_map = (input_values != reference_values).any(axis=2)
+            else:
+                self.changing_column_map = np.nan
         else:
             self.number_changing_values = None
 
@@ -338,6 +342,14 @@ class MultiModalFloatMetric(BaseMetric):
         failed_indices = np.logical_not(self.success).nonzero()
         # List all errors to terminal and file
         bad_indices_count = len(failed_indices[0])
+        if self.success.ndim == 3:
+            bad_column_count = (np.logical_not(self.success).any(axis=2) & self.changing_column_map).sum()
+            total_column_count = self.changing_column_map.sum()
+            bad_column_pct = round(bad_column_count/total_column_count * 100, 2)
+        else:
+            bad_column_count = np.nan
+            total_column_count = np.nan
+            bad_column_pct = np.nan
         full_count = len(self.references.flatten())
         failures_of_all_grid_points_pct = round(
             100.0 * (bad_indices_count / full_count), 2
@@ -346,12 +358,11 @@ class MultiModalFloatMetric(BaseMetric):
             failures_of_changing_gridpoint_pct = round(
                 100.0 * (bad_indices_count / self.number_changing_values), 2
             )
-            report_local_failures = f"Failures (changing grid points) ({bad_indices_count}/{self.number_changing_values}) ({failures_of_changing_gridpoint_pct}%)\n"
+            report_local_failures = f"changing grid points: {bad_indices_count}/{self.number_changing_values} - {failures_of_changing_gridpoint_pct}%; changing columns: {bad_column_count}/{total_column_count} - {bad_column_pct}%; all grid points: {bad_indices_count}/{full_count} - {failures_of_all_grid_points_pct}%\n"
         else:
             report_local_failures = ""
         report = [
             f"{report_local_failures}"
-            f"Failures (all grid points) ({bad_indices_count}/{full_count}) ({failures_of_all_grid_points_pct}%)\n",
             f"Index   Computed   Reference   "
             f"{'🔶 ' if not self.absolute_eps.is_default else ''}Absolute E(<{self.absolute_eps.value:.2e})  "
             f"{'🔶 ' if not self.relative_fraction.is_default else ''}Relative E(<{self.relative_fraction.value * 100:.2e}%)   "