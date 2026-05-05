0131b9715ced008ee0491f8fecffd484e57803d3
Frank Malatino <frank.malatino@noaa.gov>
2026-05-05 14:53:47 -0400

    Adding back in assert of Path in test_dm_monitor_single

--- tests/monitor/test_dm_monitor_single.py
+++ tests/monitor/test_dm_monitor_single.py
@@ -234,7 +234,7 @@ def test_dm_monitor_single_tile() -> None:
     print("Monitor cleaned up")
 
     # check output!
-    # assert Path("diag_manager_single_tile.nc").exists()
+    assert Path("diag_manager_single_tile.nc").exists()
     # ds = xr.open_dataset("diag_manager_single_tile.nc", decode_times=True)
     # print("Dataset opened through xarray")
     # assert "var_2d" in ds
