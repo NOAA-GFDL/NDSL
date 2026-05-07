2b83d845d8436085ed37103fc9d3baa41c7f4f35
Frank Malatino <frank.malatino@noaa.gov>
2026-05-07 13:12:38 -0400

    Using dask scheduler to avoid race condition

--- tests/monitor/test_dm_monitor_single.py
+++ tests/monitor/test_dm_monitor_single.py
@@ -11,6 +11,7 @@ import numpy as np
 import pytest
 import xarray as xr
 import yaml
+import dask
 
 from ndsl import (
     DiagManagerMonitor,
@@ -235,32 +236,32 @@ def test_dm_monitor_single_tile() -> None:
 
     # check output!
     assert Path("diag_manager_single_tile.nc").exists()
-    # ds = xr.open_dataset("diag_manager_single_tile.nc", decode_times=True)
-    ds = xr.open_dataset("diag_manager_single_tile.nc")
-    # print("Dataset opened through xarray")
-    # assert "var_2d" in ds
-    # np.testing.assert_array_equal(ds["var_2d"].shape, (ntimesteps, nx, ny))
-    # assert ds["var_2d"].dims == ("time", "y", "x")
-    # assert ds["var_2d"].attrs["units"] == "muntin"
-    # assert ds["var_3d"].dims == ("time", "z", "y", "x")
-    # assert ds["var_3d"].attrs["units"] == "muntin"
-    # assert ds["time"].shape == (ntimesteps,)
-    # assert ds["time"].dims == ("time",)
-    # assert ds["time"].values[0] == cftime.DatetimeNoLeap(2, 1, 1, 2, 1, 1)
-    # assert ds["time"].values[1] == cftime.DatetimeNoLeap(2, 1, 1, 3, 1, 1)
-    # assert ds["time"].values[2] == cftime.DatetimeNoLeap(2, 1, 1, 4, 1, 1)
-    # np.testing.assert_array_equal(ds["var_2d"].values[0, :, :], var2_global.transpose())
-    # np.testing.assert_array_equal(ds["var_2d"].values[1, :, :], var2_global.transpose())
-    # np.testing.assert_array_equal(ds["var_2d"].values[2, :, :], var2_global.transpose())
-    # # data is transposed when passed into fortran
-    # np.testing.assert_array_equal(
-    #     ds["var_3d"].values[0, :, :, :], var3_global.transpose()
-    # )
-    # np.testing.assert_array_equal(
-    #     ds["var_3d"].values[1, :, :, :], var3_global.transpose()
-    # )
-    # np.testing.assert_array_equal(
-    #     ds["var_3d"].values[2, :, :, :], var3_global.transpose()
-    # )
+    with dask.config.set(scheduler='synchronous'):
+        ds = xr.open_dataset("diag_manager_single_tile.nc", decode_times=True)
+    print("Dataset opened through xarray")
+    assert "var_2d" in ds
+    np.testing.assert_array_equal(ds["var_2d"].shape, (ntimesteps, nx, ny))
+    assert ds["var_2d"].dims == ("time", "y", "x")
+    assert ds["var_2d"].attrs["units"] == "muntin"
+    assert ds["var_3d"].dims == ("time", "z", "y", "x")
+    assert ds["var_3d"].attrs["units"] == "muntin"
+    assert ds["time"].shape == (ntimesteps,)
+    assert ds["time"].dims == ("time",)
+    assert ds["time"].values[0] == cftime.DatetimeNoLeap(2, 1, 1, 2, 1, 1)
+    assert ds["time"].values[1] == cftime.DatetimeNoLeap(2, 1, 1, 3, 1, 1)
+    assert ds["time"].values[2] == cftime.DatetimeNoLeap(2, 1, 1, 4, 1, 1)
+    np.testing.assert_array_equal(ds["var_2d"].values[0, :, :], var2_global.transpose())
+    np.testing.assert_array_equal(ds["var_2d"].values[1, :, :], var2_global.transpose())
+    np.testing.assert_array_equal(ds["var_2d"].values[2, :, :], var2_global.transpose())
+    # data is transposed when passed into fortran
+    np.testing.assert_array_equal(
+        ds["var_3d"].values[0, :, :, :], var3_global.transpose()
+    )
+    np.testing.assert_array_equal(
+        ds["var_3d"].values[1, :, :, :], var3_global.transpose()
+    )
+    np.testing.assert_array_equal(
+        ds["var_3d"].values[2, :, :, :], var3_global.transpose()
+    )
 
     pyfms.fms.end()
