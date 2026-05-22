89294d2b5e520771a7f8af1d708ac36c181208f7
Florian Deconinck <deconinck.florian@gmail.com>
2026-05-22 14:23:32 -0400

    Add scalarized array to tree statistics

--- ndsl/dsl/dace/stree/optimizations/statistics.py
+++ ndsl/dsl/dace/stree/optimizations/statistics.py
@@ -34,20 +34,22 @@ class CountCartesianLoops(stree.ScheduleNodeVisitor):
 class CountTransient(stree.ScheduleNodeVisitor):
     def __init__(self) -> None:
         super().__init__()
-        self._counts = [0, 0, 0, 0]
+        self._counts = [0, 0, 0, 0, 0]
 
     def visit_ScheduleTreeRoot(self, node: stree.ScheduleTreeRoot) -> None:
         for data in node.containers.values():
             non_atomic_dims_count = sum(1 for x in data.shape if x != 1)
             if isinstance(data, dace.data.Array) and data.transient:
-                if non_atomic_dims_count == 1:
+                if non_atomic_dims_count == 0:
                     self._counts[0] += 1
-                elif non_atomic_dims_count == 2:
+                elif non_atomic_dims_count == 1:
                     self._counts[1] += 1
-                elif non_atomic_dims_count == 3:
+                elif non_atomic_dims_count == 2:
                     self._counts[2] += 1
-                else:
+                elif non_atomic_dims_count == 3:
                     self._counts[3] += 1
+                else:
+                    self._counts[4] += 1
 
 
 class TreeOptimizationStatistics:
@@ -59,7 +61,9 @@ class TreeOptimizationStatistics:
 
         cartesian_maps: list[int] = dataclasses.field(default_factory=lambda: [0, 0, 0])
         cartesian_fors: list[int] = dataclasses.field(default_factory=lambda: [0, 0, 0])
-        transients: list[int] = dataclasses.field(default_factory=lambda: [0, 0, 0, 0])
+        transients: list[int] = dataclasses.field(
+            default_factory=lambda: [0, 0, 0, 0, 0]
+        )
 
     def __init__(self) -> None:
         self._original_record = TreeOptimizationStatistics.Record()
@@ -93,5 +97,5 @@ class TreeOptimizationStatistics:
         msg = "Tree optimization:\n"
         msg += f"  Cartesian maps [I, J, K]: {self._original_record.cartesian_maps} -> {self._optimized_record.cartesian_maps}\n"
         msg += f"  Cartesian fors [I, J, K]: {self._original_record.cartesian_fors} -> {self._optimized_record.cartesian_fors}\n"
-        msg += f"  Transients [1D, 2D, 3D, 4D+]: {self._original_record.transients} -> {self._optimized_record.transients}\n"
+        msg += f"  Transients [Scalarized Array, 1D, 2D, 3D, 4D+]: {self._original_record.transients} -> {self._optimized_record.transients}\n"
         return msg
