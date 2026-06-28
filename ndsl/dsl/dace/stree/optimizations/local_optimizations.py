e31d1d7517f9508b14eff31c2a44fbda3ca23f4b
Roman Cattaneo <>
2026-06-28 22:08:10 +0200

    fix: new algo for creating labeled sections

--- ndsl/dsl/dace/stree/optimizations/local_optimizations.py
+++ ndsl/dsl/dace/stree/optimizations/local_optimizations.py
@@ -28,7 +28,7 @@ class _LabeledSection(tn.ScheduleTreeScope):
         return result + super().as_string(indent)
 
 
-class _LabelSections(tn.ScheduleNodeVisitor):
+class _LabelSections(tn.ScheduleNodeTransformer):
     """
     Transform entry/exit labeler nodes into a `LabeledSection` (see above)
     for easier later handling in case of local optimizations. Handles nested
@@ -77,52 +77,159 @@ class _LabelSections(tn.ScheduleNodeVisitor):
     def __str__(self) -> str:
         return "_LabelSections"
 
-    def visit_LibraryCall(self, node: tn.LibraryCall) -> None:
-        if node.node.name != "NDSLRuntime_Label":
-            # Only look at "our" label nodes.
-            return
+    def _label_marked_sections(self, scope: tn.ScheduleTreeScope) -> None:
+        """
+        This is the function that actually does all the work by going over the children of a given schedule tree
+        scope and re-grouping them into labeled sections based on `NDSLRuntime_Label` entry/exit nodes.
+        """
+        # The stack of entry nodes. They pop when the matching exit node is reached. Using a stack adds
+        # support for nested labeled sections.
+        entry_nodes_stack: list[tn.LibraryCall] = []
 
-        if node.node.unique_name.startswith("Enter__"):
-            # Keep taps on where we start.
-            self._entry_nodes.append(node)
-            return
+        # The stack of children. Every new entry node pushes its children into a new stack entry. This allows
+        # one pass to gather nested children.
+        children_stack: list[list[tn.ScheduleTreeNode]] = []
 
-        if node.node.unique_name.startswith("Exit__"):
-            # Find the matching entry node.
-            section_start = self._entry_nodes.pop()
+        # Top-level stack is for the current scope.
+        children_stack.append([])
+
+        for child in scope.children:
+            # Unless we are dealing with `tn.LibraryCall` nodes, we push all nodes to the stack of new children.
+            if not isinstance(child, tn.LibraryCall):
+                children_stack[-1].append(child)
+                continue
+
+            if not child.node.name == "NDSLRuntime_Label":
+                # Leave other library call nodes alone.
+                children_stack[-1].append(child)
+                continue
+
+            if child.node.unique_name.startswith("Enter__"):
+                # Keep taps on where we start and open a new list of children.
+                entry_nodes_stack.append(child)
+                children_stack.append([])
+                continue
+
+            # Expect to find an exit node now (matching the entry node that current on top of the stack).
+            if not child.node.unique_name.startswith("Exit__"):
+                raise RuntimeError(
+                    f"Unexpected `NDSLRuntim_Label` '{child.node.unique_name}'."
+                )
+
+            # For exit nodes, find the matching entry node and the new children.
+            section_start = entry_nodes_stack.pop()
+            new_children = children_stack.pop()
 
             # sanity checks
             # - ensure we have the right section (if not, something is screwed up)
             name = section_start.node.unique_name.removeprefix("Enter__")
-            exit_name = node.node.unique_name.removeprefix("Exit__")
-            assert name == exit_name
+            assert name == child.node.unique_name.removeprefix("Exit__")
             # - ensure we have the same parent (if not something is screwed up)
             parent = section_start.parent
-            assert parent == node.parent
+            assert parent == child.parent
+            # - ensure the stack of children is not empty (it will at least contain the top-level scope)
+            assert len(children_stack) > 0
 
-            # Grab all the nodes in-between and put them in a `LabeledSection`.
-            start_index = list_index(parent.children, section_start)
-            end_index = list_index(parent.children, node)
+            # Put all the new children in a `LabeledSection` and push that into the
+            # new children of the above stack of children.
             new_node = _LabeledSection(
-                children=[
-                    child for child in parent.children[start_index + 1 : end_index]
-                ],
+                children=new_children,
                 parent=parent,
                 label=name,
-                optimizations=node.node._local_optimizations,
+                optimizations=section_start.node._local_optimizations,
             )
+            # re-parent new children to new node
+            for c in new_node.children:
+                c.parent = new_node
+            # push new node into enclosing stack of children
+            children_stack.append(new_node)
 
-            # Overwrite the nodes (including the labels) with the new node.
-            parent.children[start_index : end_index + 1] = [new_node]
+            # and - of course - the final book keeping
+            self._labeled_sections += 1
 
-    def visit_ScheduleTreeRoot(self, node: tn.ScheduleTreeRoot) -> None:
-        # Reset the stack of entry nodes.
-        self._entry_nodes = []
+        # set the new children on the current scope
+        scope.children = children_stack.pop()
 
+        # some sanity checks
+        assert len(children_stack) == 0  # expect empty stack
+        for child in scope.children:
+            assert child.parent == scope  # expect correct parent
+
+    def visit_ScheduleTreeRoot(self, node: tn.ScheduleTreeRoot) -> tn.ScheduleTreeRoot:
+        self._labeled_sections = 0
+
+        # recurse down first to label sections "leaf first"
         self.generic_visit(node)
+        self._label_marked_sections(node)
 
-        # If we have nodes left, something is screwed up.
-        assert len(self._entry_nodes) == 0
+        ndsl_log.debug(f"{self}: labeled {self._labeled_sections} sections.")
+        return node
+
+    def visit_GBlock(self, node: tn.GBlock) -> tn.GBlock:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_LoopScope(self, node: tn.LoopScope) -> tn.LoopScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_ForScope(self, node: tn.ForScope) -> tn.ForScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_WhileScope(self, node: tn.WhileScope) -> tn.WhileScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_DoWhileScope(self, node: tn.DoWhileScope) -> tn.DoWhileScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_IfScope(self, node: tn.IfScope) -> tn.IfScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_StateIfScope(self, node: tn.StateIfScope) -> tn.StateIfScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_ElifScope(self, node: tn.ElifScope) -> tn.ElifScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_ElseScope(self, node: tn.ElseScope) -> tn.ElseScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_MapScope(self, node: tn.MapScope) -> tn.MapScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
+
+    def visit_ConsumeScope(self, node: tn.ConsumeScope) -> tn.ConsumeScope:
+        self.generic_visit(node)
+        self._label_marked_sections(node)
+
+        return node
 
 
 class _ApplyLocalOptimizations(tn.ScheduleNodeVisitor):
