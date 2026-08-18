969c4157781d60d2da447621541a495f91651af7
Tobias Wicky-Pfund <wicky.tob@gmail.com>
2026-08-18 15:18:46 +0200

    lint

--- ndsl/stencils/__init__.py
+++ ndsl/stencils/__init__.py
@@ -11,28 +11,28 @@ from .basic_operations import (
     dim,
     divide,
     divide_2d,
-    divide_to_self,
-    divide_to_self_2d,
+)
+from .basic_operations import divide_to_self
+from .basic_operations import divide_to_self as divide_self
+from .basic_operations import divide_to_self_2d
+from .basic_operations import divide_to_self_2d as divide_self_2d
+from .basic_operations import (
     multiply,
     multiply_2d,
     multiply_to_self,
     multiply_to_self_2d,
     select_k,
-    set_IJ_mask_value,
-    set_value,
-    set_value_2D,
-    sign,
-    subtract,
-    subtract_2d,
-    subtract_to_self,
-    subtract_to_self_2d,
 )
-from .basic_operations import divide_to_self as divide_self
-from .basic_operations import subtract_to_self as subtract_from_self
-from .basic_operations import divide_to_self_2d as divide_self_2d
-from .basic_operations import set_value_2D as set_value_2d
-from .basic_operations import subtract_to_self_2d as subtract_from_self_2d
+from .basic_operations import set_IJ_mask_value
 from .basic_operations import set_IJ_mask_value as set_boolean_value_2d
+from .basic_operations import set_value
+from .basic_operations import set_value_2D
+from .basic_operations import set_value_2D as set_value_2d
+from .basic_operations import sign, subtract, subtract_2d
+from .basic_operations import subtract_to_self
+from .basic_operations import subtract_to_self as subtract_from_self
+from .basic_operations import subtract_to_self_2d
+from .basic_operations import subtract_to_self_2d as subtract_from_self_2d
 from .corners import FillCornersBGrid
 
 
