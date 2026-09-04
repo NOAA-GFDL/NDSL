b8e49d523d8bfae76df0d82321d962dad18a63c8
Roman Cattaneo <romanc@users.noreply.github.com>
2026-09-04 11:03:23 +0200

    Disable "LiftTrivialIf" in simplify after stree -> sdfg
    We know `LiftTrivialIf` takes longish to run on bigger graphs. At the
    same time, we expect the potential gains from using the pass to be
    minimal. We thus think it's not worth to run that pass and disable it.

--- ndsl/dsl/dace/builder/optimize.py
+++ ndsl/dsl/dace/builder/optimize.py
@@ -79,7 +79,8 @@ def _simplify(
         # We disable ScalarToSymbolPromotion because it might push symbols onto edges
         # that DaCe itself can't parse anymore later, e.g. casts,  inlined function
         # calls or (complicated) field accesses.
-        # We disable LiftTrivialIf because it takes forever on larger graphs
+        # We disable LiftTrivialIf because it takes long on bigger graphs and we estimate
+        # the potential speed gains to be minimal anyway.
         skip={"ScalarToSymbolPromotion", "LiftTrivialIf"},
     )
 
@@ -91,10 +92,16 @@ def _tree_as_sdfg(stree: tn.ScheduleTreeRoot) -> SDFG:
     This function wraps `stree.as_sdfg()` with a configuration that is suitable for
     NDSL, e.g. skipping certain passes of `sdfg.simplify()`.
     """
+    # We disable ScalarToSymbolPromotion because it might push symbols onto edges
+    # that DaCe itself can't parse anymore later, e.g. casts,  inlined function
+    # calls or (complicated) field accesses.
+    # We disable ControlFlowRaising because tree -> sdfg outputs control flow graphs.
+    # We disable LiftTrivialIf because it takes long on bigger graphs and we estimate
+    # the potential speed gains to be minimal anyway.
     return stree.as_sdfg(
         validate=False,
         simplify=False,  # D_SW failed validation on merging
-        skip={"ScalarToSymbolPromotion", "ControlFlowRaising"},
+        skip={"ScalarToSymbolPromotion", "ControlFlowRaising", "LiftTrivialIf"},
     )
 
 
